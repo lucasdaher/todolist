6c218fba3b0408d172071843b17870cd5cd877db
Lucas Daher <contato@rehadmusic.com.br>
2024-02-04 02:31:19 -0300

    feat(modal): add styles and modal states.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Style
 import "./styles.scss";
@@ -6,13 +6,38 @@ import "./styles.scss";
 // Components
 import Header from "../components/Header/Header";
 import Title from "../components/Title/Title";
+import Modal from "../components/Modal/Modal";
 
 const App = () => {
+  const [hideModal, setHideModal] = useState(true);
+  const [modalType, setModalType] = useState("editar");
+
+  const [desc, setDesc] = useState("Colocar as descrições da tarefa aqui.");
+
+  const handleOpenModal = () => {
+    if (hideModal) {
+      setHideModal(false);
+    } else {
+      setHideModal(true);
+    }
+  };
+
   return (
     <div className="app">
       <Header />
 
       <Title text="Otimize seu tempo e se organize com o nosso planejador diário." />
+
+      <button type="button" onClick={handleOpenModal}>
+        Abrir Modal
+      </button>
+
+      <Modal
+        action="excluir"
+        hideModal={hideModal}
+        desc={desc}
+        modalType={modalType}
+      />
     </div>
   );
 };
